4407976e21576725aff9a2c84a6c3c6428d9679a
annaship <tetya-loshad@yandex.ru>
2016-06-25 15:06:40 -0400

    index

--- illumina_submission/illumina_submission/urls.py
+++ illumina_submission/illumina_submission/urls.py
@@ -1,3 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^illumina_submission_py/', include('illumina_submission_py.urls')),
+    url(r'^admin/', admin.site.urls),
+]
+
+# default:
+
 """illumina_submission URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +23,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+# from django.conf.urls import url
+# from django.contrib import admin
+#
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+# ]
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
