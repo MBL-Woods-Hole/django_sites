7c92d1fe459dab0d5573166d9eacfdbf2be86d7f
annaship <tetya-loshad@yandex.ru>
2016-06-30 11:28:55 -0400

    2 db for read, doesn't work

--- mysite/submission/db_router.py
+++ mysite/submission/db_router.py
@@ -2,16 +2,55 @@
 # import settings
 
 class submissionRouter(object): 
+  
     def db_for_read(self, model, **hints):
-        "Point all operations on submission models to 'local_env454'"
+      """
+      Send reads on a specific model to the appropriate db
+      """
+      db = 'default'
+
+      """
+      This snippet is particular to my own use case - the databases I'm reporting on 
+      have tables with unique prefixes, so I'm able to find that prefix in the 
+      model._meta.db_table value to determine which database I need to point to.
+
+      But you'll probably need to solve this problem in a different way.  Take a 
+      look at this page for some other model._meta attributes you can use:
+      https://docs.djangoproject.com/en/dev/ref/models/options/
+      """
+      import re
+      one = re.search('firstartist_', model._meta.db_table)
+      two = re.search('secondartist_', model._meta.db_table)
+      if g:
+        db = 'dbone'
+      if p:
+        db = 'dbtwo'
+      return db
+  
+    def db_for_read(self, model, **hints):
+        """
+        From http://www.mechanicalgirl.com/post/reporting-django-multi-db-support/
+        Send reads on a specific model to the appropriate db
+        """
+        db = 'default'
+
+        # if model._meta.app_label.startswith('Env454'):
+        #   db = 'local_env454'
         if model._meta.app_label == 'submission':
-            return 'local_env454'
+          if model._meta.db_table.startswith('Vamps'):
+            db = 'local_vamps'
+          else:
+            db = 'local_env454'
+      
+        # "Point all operations on submission models to 'local_env454'"
         # if model._meta.app_label == 'submission':
         #     return 'local_env454'
-        return 'default'
+        return db
 
     def db_for_write(self, model, **hints):
         "Point all operations on submission models to 'local_env454'"
+        if model._meta.app_label == 'submission':
+            return None
         return 'default'
 
     def allow_relation(self, obj1, obj2, **hints):
--- mysite/submission/models_l_env454.py
+++ mysite/submission/models_l_env454.py
@@ -10,7 +10,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Contact(models.Model):
+class Env454_Contact(models.Model):
     contact_id = models.SmallIntegerField(primary_key=True)
     contact = models.CharField(max_length=32)
     email = models.CharField(max_length=64)
@@ -28,7 +28,7 @@ class Contact(models.Model):
         return self.contact
 
 
-class Dataset(models.Model):
+class Env454_Dataset(models.Model):
     dataset_id = models.SmallIntegerField(primary_key=True)
     dataset = models.CharField(unique=True, max_length=64)
     dataset_description = models.CharField(max_length=100)
@@ -41,7 +41,7 @@ class Dataset(models.Model):
         return self.dataset
 
 
-class DnaRegion(models.Model):
+class Env454_DnaRegion(models.Model):
     dna_region_id = models.AutoField(primary_key=True)
     dna_region = models.CharField(unique=True, max_length=32)
 
@@ -52,7 +52,7 @@ class DnaRegion(models.Model):
     def __str__(self):
         return self.dna_region
 
-class EnvSampleSource(models.Model):
+class Env454_EnvSampleSource(models.Model):
     env_sample_source_id = models.IntegerField(primary_key=True)
     env_source_name = models.CharField(unique=True, max_length=50)
 
@@ -65,7 +65,7 @@ class EnvSampleSource(models.Model):
         db_table = 'env_sample_source'
 
 
-class IlluminaAdaptor(models.Model):
+class Env454_IlluminaAdaptor(models.Model):
     illumina_adaptor_id = models.SmallIntegerField(primary_key=True)
     illumina_adaptor = models.CharField(unique=True, max_length=3)
 
@@ -74,11 +74,11 @@ class IlluminaAdaptor(models.Model):
         db_table = 'illumina_adaptor'
 
 
-class IlluminaAdaptorRef(models.Model):
-    illumina_adaptor = models.ForeignKey(IlluminaAdaptor, models.DO_NOTHING)
+class Env454_IlluminaAdaptorRef(models.Model):
+    illumina_adaptor = models.ForeignKey(Env454_IlluminaAdaptor, models.DO_NOTHING)
     illumina_index = models.ForeignKey('IlluminaIndex', models.DO_NOTHING)
     illumina_run_key = models.ForeignKey('IlluminaRunKey', models.DO_NOTHING)
-    dna_region = models.ForeignKey(DnaRegion, models.DO_NOTHING)
+    dna_region = models.ForeignKey(Env454_DnaRegion, models.DO_NOTHING)
     domain = models.CharField(max_length=9, blank=True, null=True)
 
     class Meta:
@@ -87,7 +87,7 @@ class IlluminaAdaptorRef(models.Model):
         unique_together = (('illumina_adaptor', 'dna_region', 'domain'),)
 
 
-class IlluminaIndex(models.Model):
+class Env454_IlluminaIndex(models.Model):
     illumina_index_id = models.AutoField(primary_key=True)
     illumina_index = models.CharField(unique=True, max_length=6)
 
@@ -96,7 +96,7 @@ class IlluminaIndex(models.Model):
         db_table = 'illumina_index'
 
 
-class IlluminaRunKey(models.Model):
+class Env454_IlluminaRunKey(models.Model):
     illumina_run_key_id = models.AutoField(primary_key=True)
     illumina_run_key = models.CharField(unique=True, max_length=5)
 
@@ -105,7 +105,7 @@ class IlluminaRunKey(models.Model):
         db_table = 'illumina_run_key'
 
 
-class Primer(models.Model):
+class Env454_Primer(models.Model):
     primer_id = models.SmallIntegerField(primary_key=True)
     primer = models.CharField(unique=True, max_length=16)
     direction = models.CharField(max_length=1)
@@ -120,7 +120,7 @@ class Primer(models.Model):
         db_table = 'primer'
 
 
-class PrimerSuite(models.Model):
+class Env454_PrimerSuite(models.Model):
     primer_suite_id = models.SmallIntegerField(primary_key=True)
     primer_suite = models.CharField(unique=True, max_length=25)
 
@@ -129,31 +129,31 @@ class PrimerSuite(models.Model):
         db_table = 'primer_suite'
 
 
-class Project(models.Model):
+class Env454_Project(models.Model):
     project_id = models.SmallIntegerField(primary_key=True)
     project = models.CharField(unique=True, max_length=32)
     title = models.CharField(max_length=64)
     project_description = models.CharField(max_length=255)
     rev_project_name = models.CharField(unique=True, max_length=32)
     funding = models.CharField(max_length=64)
-    env_sample_source = models.ForeignKey(EnvSampleSource, models.DO_NOTHING)
-    contact = models.ForeignKey(Contact, models.DO_NOTHING)
+    env_sample_source = models.ForeignKey(Env454_EnvSampleSource, models.DO_NOTHING)
+    contact = models.ForeignKey(Env454_Contact, models.DO_NOTHING)
 
     class Meta:
         managed = False
         db_table = 'project'
 
 
-class RefPrimerSuitePrimer(models.Model):
-    primer_suite = models.ForeignKey(PrimerSuite, models.DO_NOTHING)
-    primer = models.ForeignKey(Primer, models.DO_NOTHING)
+class Env454_RefPrimerSuitePrimer(models.Model):
+    primer_suite = models.ForeignKey(Env454_PrimerSuite, models.DO_NOTHING)
+    primer = models.ForeignKey(Env454_Primer, models.DO_NOTHING)
 
     class Meta:
         managed = False
         db_table = 'ref_primer_suite_primer'
 
 
-class Run(models.Model):
+class Env454_Run(models.Model):
     run_id = models.SmallIntegerField(primary_key=True)
     run = models.CharField(max_length=16)
     run_prefix = models.CharField(max_length=7)
@@ -166,19 +166,19 @@ class Run(models.Model):
         unique_together = (('run', 'platform'),)
 
 
-class RunInfo(models.Model):
+class Env454_RunInfo(models.Model):
     run_info_id = models.SmallIntegerField(primary_key=True)
     run_key = models.ForeignKey('RunKey', models.DO_NOTHING)
-    run = models.ForeignKey(Run, models.DO_NOTHING)
+    run = models.ForeignKey(Env454_Run, models.DO_NOTHING)
     lane = models.IntegerField()
     direction = models.CharField(max_length=4, blank=True, null=True)
-    dataset = models.ForeignKey(Dataset, models.DO_NOTHING)
-    project = models.ForeignKey(Project, models.DO_NOTHING)
+    dataset = models.ForeignKey(Env454_Dataset, models.DO_NOTHING)
+    project = models.ForeignKey(Env454_Project, models.DO_NOTHING)
     tubelabel = models.CharField(max_length=32)
     barcode = models.CharField(max_length=4)
     adaptor = models.CharField(max_length=1, blank=True, null=True)
     pool = models.CharField(max_length=32)
-    dna_region = models.ForeignKey(DnaRegion, models.DO_NOTHING)
+    dna_region = models.ForeignKey(Env454_DnaRegion, models.DO_NOTHING)
     amp_operator = models.CharField(max_length=5)
     seq_operator = models.CharField(max_length=5)
     empcr_operator = models.CharField(max_length=5)
@@ -192,17 +192,17 @@ class RunInfo(models.Model):
         unique_together = (('run', 'run_key', 'lane', 'direction', 'platform'),)
 
 
-class RunInfoIll(models.Model):
+class Env454_RunInfoIll(models.Model):
     run_info_ill_id = models.AutoField(primary_key=True)
     run_key = models.ForeignKey('RunKey', models.DO_NOTHING)
-    run = models.ForeignKey(Run, models.DO_NOTHING)
+    run = models.ForeignKey(Env454_Run, models.DO_NOTHING)
     lane = models.IntegerField()
-    dataset = models.ForeignKey(Dataset, models.DO_NOTHING)
-    project = models.ForeignKey(Project, models.DO_NOTHING)
+    dataset = models.ForeignKey(Env454_Dataset, models.DO_NOTHING)
+    project = models.ForeignKey(Env454_Project, models.DO_NOTHING)
     tubelabel = models.CharField(max_length=32)
     barcode = models.CharField(max_length=4)
     adaptor = models.CharField(max_length=3)
-    dna_region = models.ForeignKey(DnaRegion, models.DO_NOTHING)
+    dna_region = models.ForeignKey(Env454_DnaRegion, models.DO_NOTHING)
     amp_operator = models.CharField(max_length=5)
     seq_operator = models.CharField(max_length=5)
     barcode_index = models.CharField(max_length=12)
@@ -210,7 +210,7 @@ class RunInfoIll(models.Model):
     insert_size = models.SmallIntegerField()
     file_prefix = models.CharField(max_length=45)
     read_length = models.SmallIntegerField()
-    primer_suite = models.ForeignKey(PrimerSuite, models.DO_NOTHING)
+    primer_suite = models.ForeignKey(Env454_PrimerSuite, models.DO_NOTHING)
     updated = models.DateTimeField()
     platform = models.CharField(max_length=7, blank=True, null=True)
 
@@ -220,7 +220,7 @@ class RunInfoIll(models.Model):
         unique_together = (('run', 'run_key', 'barcode_index', 'lane'),)
 
 
-class RunKey(models.Model):
+class Env454_RunKey(models.Model):
     run_key_id = models.SmallIntegerField(primary_key=True)
     run_key = models.CharField(unique=True, max_length=25)
 
