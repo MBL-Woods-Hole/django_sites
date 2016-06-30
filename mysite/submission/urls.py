593ddfcf8c06936d7d35adc59bcc0b43f3047ec4
annaship <tetya-loshad@yandex.ru>
2016-06-30 15:05:11 -0400

    name spacing

--- mysite/submission/urls.py
+++ mysite/submission/urls.py
@@ -2,14 +2,16 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'submission'
+
 urlpatterns = [
-    # ex: /polls/
+    # ex: /submission/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
+    # ex: /submission/5/
     url(r'^(?P<run_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
+    # ex: /submission/5/results/
     url(r'^(?P<run_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
+    # ex: /submission/5/vote/
     url(r'^(?P<run_id>[0-9]+)/vote/$', views.vote, name='vote'),
     url(r'^help/$', views.help, name='help'),
 ]
