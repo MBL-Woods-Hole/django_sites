f6c033c04a65a80661a0953643bb3db821fe8133
annaship <tetya-loshad@yandex.ru>
2016-06-30 14:47:03 -0400

    run render

--- mysite/submission/views.py
+++ mysite/submission/views.py
@@ -37,23 +37,30 @@ def index(request):
 from django.http import HttpResponse
 from django.template import loader
 
+from django.shortcuts import render
 from .models_l_env454 import Run
 
 
+# def index(request):
+#     latest_run_list = Run.objects.order_by('-run')[:15]
+#     template = loader.get_template('submission/index.html')
+#     context = {
+#         'latest_run_list': latest_run_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+
 def index(request):
-    latest_run_list = Run.objects.order_by('-run')[:15]
-    template = loader.get_template('submission/index.html')
-    context = {
-        'latest_run_list': latest_run_list,
-    }
-    return HttpResponse(template.render(context, request))
-    
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    latest_run_list = Run.objects.order_by('-run')[:10]
+    context = {'latest_run_list': latest_run_list}
+    return render(request, 'submission/index.html', context)
+        
+def detail(request, run_id):
+    return HttpResponse("You're looking at run %s." % run_id)
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def results(request, run_id):
+    response = "You're looking at the results of run %s."
+    return HttpResponse(response % run_id)
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+def vote(request, run_id):
+    return HttpResponse("You're voting on run %s." % run_id)
