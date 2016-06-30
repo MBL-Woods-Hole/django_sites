1b0fa3b92506e83b4d780fa6fd633471ccb1fabb
annaship <tetya-loshad@yandex.ru>
2016-06-30 14:35:52 -0400

    help intermediate

--- mysite/submission/views.py
+++ mysite/submission/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+
+'''
 # from django.shortcuts import render
 
 # Create your views here.
@@ -6,3 +7,30 @@ def index(request):
     return HttpResponse("Illumina files processing")
     
 # def 
+'''
+#
+# from django.shortcuts import render_to_response
+# from django.template import RequestContext
+# from django.views.generic import TemplateView
+#
+# def index(request):
+#   return render_to_response('submission/index.html', context_instance=RequestContext(request))
+#
+#
+# class HelpView(TemplateView):
+#     template_name = "help.html"
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Illumina files processing")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
