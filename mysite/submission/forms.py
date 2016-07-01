4d59a83b7208dc6f9894ba2656b9c9f971d5813e
annaship <tetya-loshad@yandex.ru>
2016-07-01 16:36:58 -0400

    field select

--- mysite/submission/forms.py
+++ mysite/submission/forms.py
@@ -5,3 +5,25 @@ from django import forms
 
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
+    YEAR_IN_SCHOOL_CHOICES = (
+       ('FR', 'Freshman'),
+       ('SO', 'Sophomore'),
+       ('JR', 'Junior'),
+       ('SR', 'Senior'),
+    )
+    find_rundate = forms.ChoiceField(YEAR_IN_SCHOOL_CHOICES)
+    
+
+class ContactForm(forms.Form):
+    YEAR_IN_SCHOOL_CHOICES = (
+       ('FR', 'Freshman'),
+       ('SO', 'Sophomore'),
+       ('JR', 'Junior'),
+       ('SR', 'Senior'),
+    )
+    find_rundate = forms.ChoiceField(YEAR_IN_SCHOOL_CHOICES)
+  
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
