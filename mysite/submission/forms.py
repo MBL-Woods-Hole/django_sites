70da2e7b042de2662f80d5742d1d66a1e2dbdd1c
annaship <tetya-loshad@yandex.ru>
2016-07-01 18:19:49 -0400

    clean up - remove name form

--- mysite/submission/forms.py
+++ mysite/submission/forms.py
@@ -2,34 +2,9 @@ from django import forms
 from .models_l_env454 import Run
 from .models import Machine, Domain
 
-
-class NameForm(forms.Form):
-#   find_rundate = forms.ModelChoiceField(queryset = Run.objects.filter(run__gte='2014').order_by('-run')
-# , label='Run date', empty_label=None)
-  query = Run.objects.filter(run__gte='2015').filter(run__startswith='201').order_by('-run')
-  find_rundate = forms.ModelChoiceField(queryset = query, label='Run date', empty_label=None)
-  find_machine = forms.ChoiceField(Machine.MACHINE_CHOICES, label='Machine name')
-  find_domain  = forms.ChoiceField(Domain.DOMAIN_CHOICES, label='Domain')
-  find_lane    = forms.CharField(label='Lane number', max_length=10)
-
-    # your_name = forms.CharField(label='Your name', max_length=100)
-    # YEAR_IN_SCHOOL_CHOICES = (
-    #    ('FR', 'Freshman'),
-    #    ('SO', 'Sophomore'),
-    #    ('JR', 'Junior'),
-    #    ('SR', 'Senior'),
-    # )
-    # # find_rundate = forms.ChoiceField(YEAR_IN_SCHOOL_CHOICES)
-    # find_rundate = forms.ModelChoiceField(queryset = Run.objects.filter(run__startswith='201').order_by('-run')[:50], label='Run date', empty_label=None)
-    # find_machine = forms.ChoiceField(Machine.MACHINE_CHOICES, label='Machine name')
-
 class RunForm(forms.Form):
-  # book = forms.ModelChoiceField(queryset = Book.objects.filter(pk__in = Book.objects.order_by('-rating')[:100].values_list('pk')))
-
-  # find_rundate = forms.ModelChoiceField(queryset = Run.objects.filter(pk__in = Run.objects.filter(run__startswith='201').order_by('-run')[:50].values_list('pk')))
-
-  query = Run.objects.filter(run__gte='2015').filter(run__startswith='201').order_by('-run')
-  find_rundate = forms.ModelChoiceField(queryset =query, label='Run date', empty_label=None)
+  query = Run.objects.filter(run__startswith='201').filter(run__gte='2015').order_by('-run')
+  find_rundate = forms.ModelChoiceField(queryset = query, label='Run date', empty_label=None)
   find_machine = forms.ChoiceField(Machine.MACHINE_CHOICES, label='Machine name')
   find_domain  = forms.ChoiceField(Domain.DOMAIN_CHOICES, label='Domain')
   find_lane    = forms.CharField(label='Lane number', max_length=3)
--- mysite/submission/views.py
+++ mysite/submission/views.py
@@ -89,28 +89,7 @@ def gzip_all(request):
 # ---
 from django.http import HttpResponseRedirect
 
-from .forms import NameForm, RunForm
-
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            contact_name = request.POST.get(
-                          'your_name'
-                      , '')
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            # return HttpResponseRedirect('/submission/name/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, 'submission/name.html', {'form': form})
+from .forms import RunForm
 
 def get_run(request):
     print "Running get_run"
