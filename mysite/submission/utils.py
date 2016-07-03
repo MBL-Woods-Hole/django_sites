1e2d0e762077818efc10a1f630a815948bfb1587
annaship <tetya-loshad@yandex.ru>
2016-07-03 14:38:35 -0400

    fix full machine name in a path

--- mysite/submission/utils.py
+++ mysite/submission/utils.py
@@ -1,4 +1,5 @@
 from .forms import RunForm
+from .models import Machine
 
 def get_run(request):
     print "Running get_run from utils"
@@ -12,7 +13,9 @@ def get_run(request):
             run_data['find_machine'] = form.cleaned_data['find_machine']
             run_data['find_domain']  = form.cleaned_data['find_domain']
             run_data['find_lane']    = form.cleaned_data['find_lane']
-
+            
+            machine_choices = dict(Machine.MACHINE_CHOICES)            
+            run_data['full_machine_name'] = machine_choices[form.cleaned_data['find_machine']]
             return (form, run_data)
     # if a GET (or any other method) we'll create a blank form
     else:
