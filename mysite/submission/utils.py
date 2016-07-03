8442d4455edd0d1ef0a630a97bd8f5a97036ff6e
annaship <tetya-loshad@yandex.ru>
2016-07-03 15:22:50 -0400

    full_machine_name into method

--- mysite/submission/utils.py
+++ mysite/submission/utils.py
@@ -1,6 +1,13 @@
 from .forms import RunForm
 from .models import Machine
 
+def get_overlap(machine_name):
+    pass
+
+def get_full_macine_name(machine_name):
+    machine_choices = dict(Machine.MACHINE_CHOICES) 
+    return machine_choices[machine_name]
+
 def get_run(request):
     print "Running get_run from utils"
     run_data = {}
@@ -12,10 +19,9 @@ def get_run(request):
             run_data['find_rundate'] = form.cleaned_data['find_rundate'].run
             run_data['find_machine'] = form.cleaned_data['find_machine']
             run_data['find_domain']  = form.cleaned_data['find_domain']
-            run_data['find_lane']    = form.cleaned_data['find_lane']
-            
-            machine_choices = dict(Machine.MACHINE_CHOICES)            
-            run_data['full_machine_name'] = machine_choices[form.cleaned_data['find_machine']]
+            run_data['find_lane']    = form.cleaned_data['find_lane']            
+            run_data['full_machine_name'] = get_full_macine_name(form.cleaned_data['find_machine'])
+
             return (form, run_data)
     # if a GET (or any other method) we'll create a blank form
     else:
