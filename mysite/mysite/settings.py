aa27e936e893001b5e11c2c91a90939cd3339b09
annaship <tetya-loshad@yandex.ru>
2016-09-27 12:31:09 -0400

    use local settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,13 +11,11 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os, sys
-try:
-    from local_settings import *
-except ImportError as e:
-    pass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ILLUMINA_INFO_DIRS = ["hiseq_info", "nextseq_info", "miseq_info"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -220,3 +218,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from settings_local import *
