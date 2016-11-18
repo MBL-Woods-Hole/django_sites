32b8ea3d32e9e56b82724a2b38b303f52812e7cd
annaship <tetya-loshad@yandex.ru>
2016-11-18 16:55:53 -0500

    comments removed

--- mysite/submission/forms.py
+++ mysite/submission/forms.py
@@ -71,93 +71,6 @@ class MetadataOutCsvForm(forms.Form):
     barcode                 = forms.CharField(max_length=12, required=False)
     amp_operator            = forms.CharField(max_length=5, widget=forms.TextInput(attrs={'class': 'size_short_input'}))
     
-class MonthYearWidget(forms.MultiWidget):
-    """
-   A widget that splits a date into Month/Year with selects.
-   """
-    def __init__(self, attrs=None):
-        months = (
-            ('', 'Month'),
-            ('', '---'),
-            ('01', 'Jan (01)'),
-            ('02', 'Feb (02)'),
-            ('03', 'Mar (03)'),
-            ('04', 'Apr (04)'),
-            ('05', 'May (05)'),
-            ('06', 'Jun (06)'),
-            ('07', 'Jul (07)'),
-            ('08', 'Aug (08)'),
-            ('09', 'Sep (09)'),
-            ('10', 'Oct (10)'),
-            ('11', 'Nov (11)'),
-            ('12', 'Dec (12)'),
-        )
-
-        year_now = int(datetime.date.today().year)
-        year_digits = range(year_now, year_now + 10)
-        years = [(year, year) for year in year_digits]
-        years.insert(0, ("", "---"))
-        years.insert(0, ("", "Year"))
-
-        widgets = (forms.Select(attrs=attrs, choices=months), forms.Select(
-            attrs=attrs, choices=years))
-        super(MonthYearWidget, self).__init__(widgets, attrs)
-
-    def decompress(self, value):
-        if value:
-            return [value.month, value.year]
-        return [None, None]
-
-    def render(self, name, value, attrs=None):
-        try:
-            value = datetime.date(month=int(
-                value[0]), year=int(value[1]), day=1)
-        except:
-            value = ''
-
-        return super(MonthYearWidget, self).render(name, value, attrs)
-
-
-class MonthYearField(forms.MultiValueField):
-    def __init__(self, *args, **kwargs):
-        """
-        Have to pass a list of field types to the constructor, else we
-        won't get any data to our compress method.
-        """
-
-        all_fields = (
-            fields.CharField(),
-            fields.CharField(),
-        )
-        super(MonthYearField, self).__init__(all_fields, *args, **kwargs)
-
-    def compress(self, data_list):
-        if data_list:
-            s = data_list[0]
-            month = int(s) if s else 1
-            s = data_list[1]
-            year = int(s) if s else datetime.date.today().year
-
-            return datetime.date(year=year, month=month, day=1)
-        return None
-        
-# class NewProjectField(forms.MultiValueField):
-#     widget = MonthYearWidget
-#
-#     def __init__(self, *args, **kwargs):
-#         super(NewProjectField, self).__init__(*args, **kwargs)
-#         fields = (
-#             forms.CharField(),
-#             forms.CharField()
-#         )
-#         """
-#         The first part of a project name could have only letters and no more then 4 of them. The second part of a project name could have only letters and numbers and no more then 6 of them
-#         domain
-#         dna_region
-#         """
-#
-#     def compress(self, data_list):
-#         return ' '.join(data_list)
 
 
 class AddProjectForm(forms.Form):
@@ -174,10 +87,3 @@ class AddProjectForm(forms.Form):
     contact_name_query  = Contact.objects.all().order_by('last_name')
     contact             = forms.ModelChoiceField(queryset = contact_name_query, label = 'Contact Name', empty_label = None, to_field_name = 'contact')
     
-    # def __init__(self, *args, **kwargs):
-    #      super(AddProjectForm, self).__init__(self, *args, **kwargs)
-    #      self.initial['project'] = ['']
-    # django.core.exceptions.ImproperlyConfigured: Creating a ModelForm without either the 'fields' attribute or the 'exclude' attribute is prohibited; form AddProjectForm needs updating.
-
-    # class Meta:
-    #     model = Project
