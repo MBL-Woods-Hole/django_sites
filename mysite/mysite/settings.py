9a27bbac0950e062d34be1fb9e051091ebea74ee
annaship <tetya-loshad@yandex.ru>
2016-12-13 20:07:32 -0500

    submissions/illumina/

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -239,7 +239,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = os.path.dirname(os.path.join(BASE_DIR, 'submission/static'))
-STATIC_URL = '/static/'
+STATIC_URL = '/submissions/illumina/static/'
 
 try:
     HOSTNAME = socket.gethostname()
