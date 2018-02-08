355edb5412ad67b07e94b15d5c0dc0bbfe135d57
annaship <tetya-loshad@yandex.ru>
2018-02-08 12:28:07 -0500

    MIDDLEWARE

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
