b8cbc9cdd23a3356e4552741c229bc8084f85348
annaship <tetya-loshad@yandex.ru>
2018-11-05 12:30:38 -0500

    print_outs

--- mysite/submission/metadata_tools.py
+++ mysite/submission/metadata_tools.py
@@ -957,6 +957,10 @@ class CsvMetadata():
         for r in self.domain_dna_regions:
             domain_letter = r[0]
             dna_region = r[1:]
+            logging.info("domain_letter: %s" % domain_letter)
+            logging.debug("dna_region: %s" % dna_region)
+            logging.info("dna_region: %s" % dna_region)
+            logging.info("self.suite_domain_choices: %s" % self.suite_domain_choices)
             primer_suite = self.suite_domain_choices[domain_letter] + ' ' + dna_region.upper() + ' Suite'
             primer_suites.append(primer_suite)
         return primer_suites
