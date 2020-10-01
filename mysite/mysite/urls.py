b519b60ae44d50f1e9543dc17a0b2e54eb11d052
Andrew Voorhis <avoorhis@mbl.edu>
2020-10-01 09:09:12 -0400

    fix url bug

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
-from django.urls import include, path
 
 urlpatterns = [
     url(r'^submission/', include('submission.urls')),
