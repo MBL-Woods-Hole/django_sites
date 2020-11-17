3dc7a6f22fe61df396e9026fb6adbb71202b2409
Andrew Voorhis <avoorhis@mbl.edu>
2020-11-17 10:52:43 -0500

    fix typo

--- mysite/submission/metadata_tools.py
+++ mysite/submission/metadata_tools.py
@@ -1093,7 +1093,7 @@ class CsvMetadata():
         return (metadata_run_info_form, has_empty_cells)
 
     def get_domain_dna_regions(self, data_dict):
-         print("PPP5 data_dict: %s" % data_dict)
+        print("PPP5 data_dict: %s" % data_dict)
 
         try:
             self.domain_dna_regions = [k.split("_")[-1] for k in [x['project'] for x in data_dict]]
